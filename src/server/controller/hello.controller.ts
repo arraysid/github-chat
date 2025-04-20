fc9c301fa084f1c82ae94477e759932a13558f2c
Muharu <arraysdotid@gmail.com>
2025-04-20 13:36:54 +0700

    feat: add authentication middleware and error handling for HTTP responses

--- src/server/controller/hello.controller.ts
+++ src/server/controller/hello.controller.ts
@@ -1,9 +1,11 @@
 import { createEndpoint } from "better-call";
+import { authMiddleware } from "../middlewares";
 
 export const hello = createEndpoint(
   "/api/hello",
   {
     method: "GET",
+    use: [authMiddleware],
   },
   async () => {
     return {
