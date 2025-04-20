7fda3272cc9c72723d566f629ae67f3db5e202b8
Muharu <arraysdotid@gmail.com>
2025-04-20 08:36:23 +0700

    feat: implement dashboard layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Button>Click Me</Button>
+    <main className="flex h-screen items-center justify-center">
+      <Link href="/dashboard" className={buttonVariants()}>
+        Dashboard
+      </Link>
     </main>
   );
 }
